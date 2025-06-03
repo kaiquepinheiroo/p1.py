08f70fd3155db172df1df4f5b442cd93831e180e
kaiquepinheiroo <kaique.alcantara@estudante.ifmt.edu.br>
2025-06-03 14:44:51 +0000

    prova

---
+++ p1.py
@@ -0,0 +1,28 @@
+'''
+PROVA PRÁTICA DE PYTHON
+Ao término enviar e-mail conforme modelo:
+Para: preti.joao@ifmt.edu.br
+Assunto: Prova 1 de Laboratório de Programação 2025/1
+Mensagem: Coloque seu nome completo na mensagem do e-mail
+Anexo: prova.py
+'''
+
+#1. Faça um programa que leia o saldo de uma conta corrente,
+#   o juros de rendimento mensal e imprima o valor em reais do
+#   rendimento referente a 1 mês de juros. (2,5pt)
+
+#2. Complemente a questão 1 da prova, para que o cálculo do
+#   rendimento só ocorra se o saldo da conta for positivo.
+#   Caso contrário, deverá exibir uma mensagem informando que o
+#   rendimento não pode ser calculado para valores negativos ou sem
+#   saldo em conta corrente. (2,5pt)
+
+#3. Complemente a questão 1 ou 2 da prova, para que não seja aceito juros
+#   negativo. O programa deve reiteradamente informar o usuário do erro
+#   e solicitar novo valor para o juros. (2,5pt)
+
+#4. Complemente a questão 1, 2 ou 3 da prova, para que o cálculo
+#    do rendimento suporte um prazo maior do que 1 mês. (2,5pt)
+#    O programa deverá solicitar do usuário também prazo do
+#    investimento em meses.
+#    Atenção: trata-se de uma operação de juros composto.
